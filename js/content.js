39678a106666edfda3e6766a3c1af20aca086044
malekballer <140664425+malekballer@users.noreply.github.com>
2026-07-31 15:21:48 +0300

    Update content.js

--- js/content.js
+++ js/content.js
@@ -2,10 +2,8 @@ import { round, score } from './score.js';
 
 /**
  * Path to directory containing `_list.json` and all levels
- * Resolves to your repository subfolder path safely on GitHub Pages
  */
 const dir = './data';
-const dir = '/syrian-demon-list/data';
 
 export async function fetchList() {
     const listResult = await fetch(`${dir}/_list.json`);
@@ -50,7 +48,7 @@ export async function fetchEditors() {
 
 export async function fetchLeaderboard() {
     const list = await fetchList();
-    if (!list) return [[], []]; // Prevents "Cannot read properties of null (reading 'forEach')" crash!
+    if (!list) return [[], []]; // Prevents crash if list fails to load
 
     const scoreMap = {};
     const errs = [];
