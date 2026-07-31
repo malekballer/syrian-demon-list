7414165b1f259ac41dd82487a6a14f2106506277
malekballer <140664425+malekballer@users.noreply.github.com>
2026-07-31 17:04:52 +0300

    Update content.js

--- js/content.js
+++ js/content.js
@@ -58,22 +58,7 @@ export async function fetchLeaderboard() {
             return;
         }
 
-        // Verification
-        const verifier = Object.keys(scoreMap).find(
-            (u) => u.toLowerCase() === level.verifier.toLowerCase(),
-        ) || level.verifier;
-        scoreMap[verifier] ??= {
-            verified: [],
-            completed: [],
-            progressed: [],
-        };
-        const { verified } = scoreMap[verifier];
-        verified.push({
-            rank: rank + 1,
-            level: level.name,
-            score: score(rank + 1, 100, level.percentToQualify),
-            link: level.verification,
-        });
+        // --- VERIFIER BLOCK REMOVED SO VERIFIERS DON'T AUTO-POPULATE LEADERBOARD ---
 
         // Records
         level.records.forEach((record) => {
