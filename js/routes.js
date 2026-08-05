96f87df11ab55f02af91c8d8c5fa934068bb876a
malekballer <140664425+malekballer@users.noreply.github.com>
2026-08-05 19:22:57 +0300

    Update routes.js

--- js/routes.js
+++ js/routes.js
@@ -3,7 +3,7 @@ import Leaderboard from './pages/Leaderboard.js';
 import Roulette from './pages/Roulette.js';
 
 export default [
-    { path: '/:level?', component: List }
+    { path: '/:level?', component: List },
     { path: '/leaderboard', component: Leaderboard },
     { path: '/roulette', component: Roulette },
 ];
